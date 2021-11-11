4aba1d264871b517335d50c319ce3a77c81b5d99
nomanali630 <noman@irenictech.com>
2021-11-11 14:52:44 +0500

    new work

---
+++ learning/src/components/ExpanseItem.jsx
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+import './ExpanseItem.css'
+import ExpanseDate from './ExpanseDate'
+
+const ExpanseItem = (props) => {
+   const [title,setTitle] = useState(props.title)
+    const eventHandler = () => {
+        setTitle('kam update hogya')
+        console.log('event clicked')
+    }
+
+
+
+    return (
+        <div className='main-container'>
+            <div className='expense-item' >
+                <ExpanseDate date={props.date} />
+                <div className='expense-item__description'>
+                    <h2>{title}</h2>
+                </div>
+                <div className='expense-item__price'>${props.amount}</div>
+                <button onClick={eventHandler}>change title</button>
+            </div>
+        </div>
+    )
+}
+
+export default ExpanseItem
